0864a6d44a76849a076864b465e874b98eef04c2
saahirfoux <saahirhfoux@gmail.com>
2020-09-23 06:40:03 -0400

    Updates

---
+++ precache-manifest.c9fb04be425bd66a771064d8b1ce1fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96fd2a9a16752556a2b244503957508",
+    "url": "/charter/index.html"
+  },
+  {
+    "revision": "2b06ea8ab1cfe500330e",
+    "url": "/charter/static/css/main.d87298df.chunk.css"
+  },
+  {
+    "revision": "0f628da224c653acf7ea",
+    "url": "/charter/static/js/2.a0e6ec0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/charter/static/js/2.a0e6ec0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b06ea8ab1cfe500330e",
+    "url": "/charter/static/js/main.eaa47cd7.chunk.js"
+  },
+  {
+    "revision": "7732e3cac26ad1993778",
+    "url": "/charter/static/js/runtime-main.eba08f44.js"
+  }
+]);
