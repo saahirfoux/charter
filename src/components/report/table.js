d399053c3b360acc34d8ce7f70c30a79e6cd35c9
saahirfoux <saahirhfoux@gmail.com>
2020-09-23 05:15:45 -0400

    Added Attire Filter
    Added new stretch goal attire filter by re-using my existing component. Also renamed collectGenres to "createGenresList"

--- src/components/report/table.js
+++ src/components/report/table.js
@@ -31,7 +31,7 @@ function Body({content}) {
  * @param {*} content 
  * @returns [] Array
  */
-function collectGenres(content) {
+function createGenresList(content) {
     let genreSet = new Set();
     content.forEach((row) => {
         row.genre.split(',').forEach((genre) => {
@@ -39,6 +39,19 @@ function collectGenres(content) {
         });
     });
     return Array.from(genreSet)
+}
+
+/**
+ * @summary scans each row, splits & stores each into a set to quickly gather distinct values 
+ * @param {*} content 
+ * @returns [] Array
+ */
+function createAttireList(content) {
+    let attireSet = new Set();
+    content.forEach((row) => {
+        attireSet.add(row.attire)
+    });
+    return Array.from(attireSet)
 }
 
 /**
@@ -61,16 +74,18 @@ function getControlObj(val, control, extension) {
 }
 
 export const Table = ({content}) => {
-    const genres = collectGenres(content)
+    const genres = createGenresList(content)
+    const attire = createAttireList(content)
     const [search, setSearch] = useState('')
     const [page, setPage] = useState([0, 10])
     const [stateFilter, setStateFilter] = useState('')
     const [genreFilter, setGenreFilter] = useState('')
+    const [attireFilter, setAttireFilter] = useState('')
     const [filteredRestaurants, setFilteredRestaurants] = useState([])
 
     useEffect(() => {
         setFilteredRestaurants(content.filter( restaurant => {
-            let {name,city,state,genre} = restaurant;
+            let {name,city,state,genre, attire} = restaurant;
 
             if (!state.includes(stateFilter)) {
                 return false
@@ -80,11 +95,15 @@ export const Table = ({content}) => {
                 return false
             }
 
+            if (!attire.includes(attireFilter)) {
+                return false
+            }
+
             return (name + city + genre).toLowerCase().includes(search.toLowerCase())
         }))
         // reset pagination
         setPage([0, 10]);
-    }, [search, stateFilter, genreFilter, content])
+    }, [search, stateFilter, genreFilter, attireFilter, content])
 
     const paginate = (content) => {
         return content.length > 0 ? content.slice(page[0], page[1]) : content
@@ -95,6 +114,7 @@ export const Table = ({content}) => {
             <Search controls={getControlObj(search, setSearch)}/>
             <Filter content={states} controls={getControlObj(stateFilter, setStateFilter)}/>
             <Filter content={genres} controls={getControlObj(genreFilter, setGenreFilter)}/>
+            <Filter content={attire} controls={getControlObj(attireFilter, setAttireFilter)}/>
             <Body content={paginate(filteredRestaurants)}/>
             <Pagination content={filteredRestaurants} controls={getControlObj(page, setPage)}/>
         </div>
